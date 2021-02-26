45187ae969680929ef3c1478a98a85991f80da4b
Viktor Fedorov <fedorov.viktor.1988@gmail.com>
2021-02-26 23:17:02 +0600

    Add blank message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
-import ReactClassComponent from '../ReactClassComponent/ReactClassComponent'
-import ReactPureComponent from '../ReactPureComponent/ReactPureComponent'
-import ReactFunctionComponent from '../ReactFunctionComponent/ReactFunctionComponent'
-import ReactCreateElementComponent from '../ReactCreateElementComponent/ReactCreateElementComponent'
+import React, { Component } from "react"
+import ReactClassComponent from "../ReactClassComponent/ReactClassComponent"
+import ReactPureComponent from "../ReactPureComponent/ReactPureComponent"
+import ReactFunctionComponent from "../ReactFunctionComponent/ReactFunctionComponent"
+import ReactCreateElementComponent from "../ReactCreateElementComponent/ReactCreateElementComponent"
 
 export default class App extends Component {
   render() {
     return (
       <>
+        <h1>Hello World!</h1>
         <ReactClassComponent />
         <ReactPureComponent />
         <ReactFunctionComponent />
